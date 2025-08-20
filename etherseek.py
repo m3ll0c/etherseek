70214f6ec8d6cd2a9372b87de7c5aee58424bc1f
m3ll0c <gabriel.omelo@outlook.com>
2025-08-19 22:11:39 -0300

    name changed to botlib

--- etherseek.py
+++ etherseek.py
@@ -1,121 +1,118 @@
+# .·:'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''':·.
+# : :  ███████████████  █████                       █████████               █████      : :
+# : : ░░███░░░░░░░███  ░░███                       ███░░░░░███             ░░███       : :
+# : :  ░███  █ ░███████ ░███████   ██████ ████████░███    ░░░  ██████ ██████░███ █████ : :
+# : :  ░██████ ░░░███░  ░███░░███ ███░░██░░███░░██░░█████████ ███░░█████░░██░███░░███  : :
+# : :  ░███░░█   ░███   ░███ ░███░███████ ░███ ░░░ ░░░░░░░░██░██████░███████░██████░   : :
+# : :  ░███ ░   █░███ ██░███ ░███░███░░░  ░███     ███    ░██░███░░░░███░░░ ░███░░███  : :
+# : :  ██████████░░█████████ ████░░██████ █████   ░░█████████░░█████░░██████████ █████ : :
+# : : ░░░░░░░░░░  ░░░░░░░░░ ░░░░░ ░░░░░░ ░░░░░     ░░░░░░░░░  ░░░░░░ ░░░░░░░░░░ ░░░░░  : :
+# '·:..................................................................................:·'
+
 import json
 import shutil
+import argparse
+import traceback
 import multiprocessing
 import pandas as pd
 
 from uuid import uuid4 
 from pathlib import Path
+from argparse import Namespace
 
-from lib.page_inspector import PageInspector
-from lib.filter_interface import FilterInterface  
+from bot_lib.values import CHAINS, INTRO
+from bot_lib.page_inspector import PageInspector
+from bot_lib.chain_translator import ChainTranslator
+from bot_lib.transform import Transform
+from bot_lib.retriever import Retriever
 
 
 def invoke_inspector(load):
     inspector = PageInspector(headless=True)
-    return inspector.capture_requests(load)
+    return inspector.capture_requests(load[0], load[1])
 
 
-def filter_results(results, keyword):
-    result = []
+def seek(args: Namespace):
+    """Where the real work happens
 
-    for captures in results:
-        for url, data in captures.items():
-            if keyword in url:
-                result.append(captures[url])
-
-    return result
-
-def split_list(lst, n_parts):
-    """Split list into n_parts (as equal as possible)."""
-    k, m = divmod(len(lst), n_parts)
-    return [lst[i*k + min(i, m):(i+1)*k + min(i+1, m)] for i in range(n_parts)]
-
-
-def dataset_maker(filtered_results_: list):
-    dataset = {
-        "scanned_url": [],
-        "type": [],
-        "domain": [],
-        "url": [],
-        "contract_address": [],
-        "data": [],
-        "method": [],
-        "result": [],
-    }
-
-    for result_process in filtered_results_:
-        for url, data in result_process.items():
-            for request in data:
-                dataset["scanned_url"].append(url)
-                dataset["type"].append(request.get("type", ""))
-                dataset["domain"].append(request.get("domain", ""))
-                dataset["url"].append(request.get("url", ""))
-
-                if request["type"] == "request":
-                    body = json.loads(request.get("body", ""))
-                    if isinstance(body, dict):
-                        if isinstance(body.get("params"), dict):
-                            dataset["contract_address"].append(body["params"].get("to", ""))
-                            dataset["data"].append(body["params"].get("data", ""))
-                            dataset["method"].append(body.get("method", ""))
-                        elif isinstance(body.get("params"), list):
-                            for param in body["params"]:
-                                if isinstance(param, dict):
-                                    dataset["contract_address"].append(param.get("to", ""))
-                                    dataset["data"].append(param.get("data", ""))
-                                    dataset["method"].append(body.get("method", ""))
-                    elif isinstance(body, list):
-                        for req in body:
-                            if req["method"] == "eth_call":
-                                dataset["contract_address"].append(req.get("to", ""))
-                                dataset["data"].append(req.get("data", ""))
-                                dataset["method"].append(req.get("param", ""))
-                else:
-                    dataset["contract_address"].append("")
-                    dataset["data"].append("")
-                    dataset["method"].append("")
-
-                if request["type"] == "response":
-                    if request.get("body") != "<not available>":
-                        body = json.loads(request.get("body", ""))
-                        if isinstance(body, dict):
-                            dataset["result"].append(body.get("result", ""))
-                        elif isinstance(body, list):
-                            dataset["result"].append("".join([f"{res.get("result", "")};" for res in body]))
-                else:
-                    dataset["result"].append("")
-    return dataset
-                
-
-if __name__ == "__main__":
-    urls = [
-        "http://iop.med.br/exames-3",
-        "https://ceopucv.cl/wp/",
-        "https://casef.cl/",
-        "https://vivversaudepublica.com.br/tag/sistema-de-gestao/",
-        "https://www.maissauderevista.com.br/",
-        "https://celvmarilia.org.br/"
-        ]
+    Args:
+        args (Namespace): Namespace with execution arguments
+    """
+    print(args)
     
-    url_parts = split_list(urls, 3)  # split the list into 3 parts for multiprocessing
+    if "file" in args:
+        urls = Retriever.urls_from_local_file(args.file[0], args.file[1])
+    elif "urlscan" in args:
+        urls = Retriever.urls_from_urlscan(args.urlscan[0])
+
+    url_parts = Transform.split_list(urls[:10], args.jobs)
+    processing_load = [(url_part, args.verbose) for url_part in url_parts]
     
-    output_path = f"./results/{uuid4()}/"
+    output_path = f"./results/{args.output}"
+    Path(output_path).mkdir(parents=True, exist_ok=True)
 
-    with multiprocessing.Pool(processes=3) as pool:
-        net_packages = pool.map_async(invoke_inspector, url_parts).get()
+    with multiprocessing.Pool(processes=args.jobs) as pool:
+        net_packages = pool.map_async(invoke_inspector, processing_load).get()
 
-    filtered_results = filter_results(net_packages, "binance.")  # more elegant solution in the works
-
-    Path(output_path).mkdir(parents=True, exist_ok=True)  # ensure the directory exists
+    filtered_results = Transform.filter_results(net_packages, args.keyword)
 
     with open(f"{output_path}/results.json", "w+", encoding="utf-8") as f:
         f.write(json.dumps(filtered_results, indent=2))
 
-    df = pd.DataFrame(dataset_maker(filtered_results))
+    dataset = pd.DataFrame(Transform.dataset_maker(filtered_results))
+    dataset.to_csv(f"{output_path}/results.csv", index=False)
 
-    print(df.head())  # print the first few rows of the DataFrame
+    if args.wallets:
+        addresses = list(set(dataset["contract_address"].to_list()))
 
-    df.to_csv(f"{output_path}/results.csv", index=False)  # save DataFrame to CSV
+        if not args.chainid:
+            chain_name, chain_meta = ChainTranslator.translate(args.keyword)
+            wallets = Retriever.wallets(addresses, chain_meta['id'], args.wallets)
+        else:
+            wallets = Retriever.wallets(addresses, args.chainid, args.wallets)
+        
+        wallet_dataset = Transform.compact_and_add_wallet(dataset, wallets)
+        wallet_dataset.to_csv(f"{output_path}/results_with_wallets-{chain_name.replace(" ",  "-")}.csv")
+        print(wallet_dataset.head())
 
-    shutil.rmtree("./mock_data/")  # clean up the output directory after use
-    print(f"Results saved to {output_path}/results.json")
+    shutil.rmtree("./mock_data/")
+    print(f"Results saved to {output_path}")
+
+
+if __name__ == "__main__":
+    print(INTRO)
+    parser = argparse.ArgumentParser()
+
+    data_source = parser.add_mutually_exclusive_group(required=True)
+    data_source.add_argument(
+                            '-f', '--file', action="store", nargs=2,
+                            help="specify the filepath, should be a csv file, if used, column name is REQUIRED",
+                            metavar=("file","column_name") 
+                            )
+    data_source.add_argument('-u', '--urlscan', action="store", nargs=1,
+                             help="uses urlscan as a source for compromised urls (you can change the query with the config file), it needs an API KEY",
+                             metavar=("api_key") 
+                             )
+
+    output_group = parser.add_argument_group('output')
+    output_group.add_argument('-o', '--output', default=str(uuid4()), help="changes the output path, it uses a random unique identifier as default")
+    output_group.add_argument('-r', '--raw', default=False, action="store_true", help="saves the raw network data, it is a little storage expensive")
+
+    analysis_group = parser.add_argument_group('analysis')
+    analysis_group.add_argument('-k', '--keyword', default=None, required=True, help="specify the keyword for the request url, 'binance' is fully supported")
+    analysis_group.add_argument('-w', '--wallets', default=None, help="retrieves wallets from etherscan, an api key is REQUIRED, wallet analysis bypass if not specified")
+    analysis_group.add_argument('-ci', '--chainid', default=None,  type=int, help="specify the chain id to verify the wallets, it defaults to a guess based on the request domain")
+
+    process_group = parser.add_argument_group('process')
+    process_group.add_argument('-j', '--jobs', default=1,  type=int, help="specify the number of parallel processes, it is async for performance sake")
+    process_group.add_argument('-v', '--verbose', default=False, action="store_true", help="give the gruesome details, i sugest to leave it on, but the default is false")
+
+    args = parser.parse_args()
+
+    try:
+        seek(args)
+    except Exception as e:
+        if args.verbose:
+            traceback.print_exc()
+        else:
+            print(e)
--- lib/filter_interface.py
+++
@@ -1,6 +0,0 @@
-class FilterInterface:
-    def __init__(self, data):
-        self.data = data
-
-    def apply(self, criteria):
-        return [item for item in self.data if criteria(item)]
